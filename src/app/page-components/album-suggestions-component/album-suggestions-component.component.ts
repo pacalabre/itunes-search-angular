8777f886297ff0d13ec76492846a53f8ec939b78
Paul Calabrese <pacalabre@gmail.com>
2018-01-28 15:17:09 -0800

    added html structure to the albums suggestions page and made some more styles needed on that page global and removed from albums specific component

--- src/app/page-components/album-suggestions-component/album-suggestions-component.component.ts
+++ src/app/page-components/album-suggestions-component/album-suggestions-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlbumSearchService } from '../../album-search.service';
 
 @Component({
   selector: 'app-album-suggestions-component',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album-suggestions-component.component.css']
 })
 export class AlbumSuggestionsComponentComponent implements OnInit {
+  albums;
 
-  constructor() { }
+  constructor( private service: AlbumSearchService ) {
+  	this.service.getSavedAlbums();
+  	this.albums = this.service.savedAlbums;
+  	console.log("this albums are: " + this.albums);
+   }
 
   ngOnInit() {
   }
